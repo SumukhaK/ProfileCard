bb9ec49687df031799e76a371fa14ac548168d08
sumukha <sumukh.aithal@gmail.com>
2023-05-27 19:59:13 +0530

    Initial commit

---
+++ app/src/main/java/com/ksa/profilecard/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.ksa.profilecard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ksa.profilecard.ui.theme.LighterGreen
+import com.ksa.profilecard.ui.theme.ProfileCardTheme
+import com.ksa.profilecard.ui.theme.lightGreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileCardTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        ProfileCard()
+    }
+}
+
+@Composable
+fun ProfileCard(){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight(align = Alignment.Top)
+        .padding(16.dp),
+        elevation = 16.dp,
+        backgroundColor = Color.White
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            ProfilePicture()
+            ProfileContent()
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture(){
+    Card(shape = CircleShape,
+        border = BorderStroke(width = 2.dp,color = LighterGreen),
+        modifier = Modifier.padding(8.dp),
+        elevation = 4.dp) {
+        Image(painter = painterResource(id = R.drawable.profile2),
+            modifier = Modifier.size(72.dp),
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop)
+    }
+
+}
+
+@Composable
+fun ProfileContent(){
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()) {
+
+        Text(text = "John Doe", style = MaterialTheme.typography.h5)
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(text = "Active Now", style = MaterialTheme.typography.body2)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProfileCardTheme {
+        MainScreen()
+    }
+}
