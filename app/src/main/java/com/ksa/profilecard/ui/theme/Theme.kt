c12880cb5ecf3d527affded8338e80aabec9d57e
sumukha <sumukh.aithal@gmail.com>
2023-05-27 20:46:53 +0530

    added AppBar

--- app/src/main/java/com/ksa/profilecard/ui/theme/Theme.kt
+++ app/src/main/java/com/ksa/profilecard/ui/theme/Theme.kt
@@ -8,15 +8,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Teal200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Purple500
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary =  Teal200,
     primaryVariant = Purple700,
-    secondary = Teal200,
+    secondary = Purple500,
     surface = LighterGray,
 
     /* Other default colors to override
