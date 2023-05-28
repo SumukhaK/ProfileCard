dac8c76423865d139f4bd8e1e12237e49683ed76
sumukha <sumukh.aithal@gmail.com>
2023-05-28 16:42:39 +0530

    loading image from URL

--- app/src/main/java/com/ksa/profilecard/MainActivity.kt
+++ app/src/main/java/com/ksa/profilecard/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,11 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.ksa.profilecard.ui.theme.LighterGreen
 import com.ksa.profilecard.ui.theme.ProfileCardTheme
 import com.ksa.profilecard.ui.theme.lightGreen
@@ -34,16 +38,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun MainScreen(userProfiles: List<UserProfile>) {
     Scaffold(topBar = { AppBar() }) {
         Surface(modifier = Modifier
             .fillMaxSize()
             .padding(it)) {
-            Column {
-                for(user in userProfileList){
-                    ProfileCard(user)
+            LazyColumn() {
+                items(userProfiles){userProfile ->
+                    ProfileCard(userProfile = userProfile)
                 }
             }
         }
@@ -60,7 +63,7 @@ fun AppBar(){
 @Composable
 fun ProfileCard(userProfile: UserProfile){
     Card(modifier = Modifier
-        .padding(top= 2.dp, bottom =2.dp, start = 8.dp, end = 8.dp)
+        .padding(top = 2.dp, bottom = 2.dp, start = 8.dp, end = 8.dp)
         .fillMaxWidth()
         .wrapContentHeight(align = Alignment.Top)
         .padding(16.dp),
@@ -70,14 +73,14 @@ fun ProfileCard(userProfile: UserProfile){
         Row(modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start) {
-            ProfilePicture(userProfile.drawableId,userProfile.status)
+            ProfilePicture(userProfile.pictureUrl,userProfile.status)
             ProfileContent(userProfile.name,userProfile.status)
         }
     }
 }
 
 @Composable
-fun ProfilePicture(drawableId:Int,status:Boolean){
+fun ProfilePicture(drawableURL:String,status:Boolean){
     Card(shape = CircleShape,
         border = BorderStroke(width = 2.dp,color = if(status)
             LighterGreen
@@ -85,12 +88,17 @@ fun ProfilePicture(drawableId:Int,status:Boolean){
         ),
         modifier = Modifier.padding(8.dp),
         elevation = 4.dp) {
-        Image(painter = painterResource(id = drawableId),
+      /*  Image(painter = painterResource(id = drawableId),
             modifier = Modifier.size(72.dp),
             contentDescription = "Profile Picture",
-            contentScale = ContentScale.Crop)
+            contentScale = ContentScale.Crop)*/
+        AsyncImage(
+             model = drawableURL,
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = "Profile Picture",
+        )
     }
-
 }
 
 @Composable
